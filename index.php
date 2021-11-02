086a859e8ec65ded4a00ed763d99aef321e6d297
Muhammad Rifki Widadi <rifki.widadi29@gmail.com>
2021-11-02 13:07:36 +0700

    feat: add favicon website

--- index.php
+++ index.php
@@ -37,6 +37,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<link rel="apple-touch-icon" sizes="180x180" href="Assets/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="Assets/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="Assets/favicon-16x16.png">
+<link rel="manifest" href="Assets/site.webmanifest">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
