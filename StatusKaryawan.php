896fb1eab553cf319c4720e2a198ea703e3542ae
Muhammad Rifki Widadi <rifki.widadi29@gmail.com>
2021-11-02 12:01:00 +0700

    feat: add StatusKaryawan extends from Karyawan

---
+++ StatusKaryawan.php
@@ -0,0 +1,54 @@
+<?php
+    require_once('Karyawan.php');
+
+    class StatusKaryawan extends Karyawan {
+        const FULLTIME = 'fulltime';
+        const PARTTIME = 'parttime';
+        private array $dataToArray;
+
+        public function __construct(
+            $no,
+            $nama,
+            $ttl,
+            $gender,
+            $level,
+            $status
+        ) {
+            parent::__construct(
+                $no,
+                $nama,
+                $ttl,
+                $gender,
+                $level,
+                $status
+            );
+            $this->setGajiKaryawanStatus($this->getStatusKaryawan());
+            $this->dataToArray = array (
+                $this->getNoKaryawan(),
+                $this->getNamaKaryawan(),
+                $this->getTTLKaryawan(),
+                $this->getGenderKaryawan(),
+                $this->getLevelKaryawan(),
+                $this->getStatusKaryawan(),
+                $this->getGajiKaryawan()
+            );
+        }
+
+        public function getDataInArray()
+        {
+            return $this->dataToArray;
+        }
+
+        public function setGajiKaryawanStatus(string $status)
+        {
+            switch ($status) {
+                case StatusKaryawan::FULLTIME:
+                    $this->setGajiKaryawan($this->getGajiKaryawan());
+                    break;
+                case StatusKaryawan::PARTTIME:
+                    $this->setGajiKaryawan($this->getGajiKaryawan() * 0.5);
+                    break;
+            }
+        }
+    }
+?>
