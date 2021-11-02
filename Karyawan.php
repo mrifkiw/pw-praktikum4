6aa9f072a7d64864635b4abddb9e9a18ed442164
Muhammad Rifki Widadi <rifki.widadi29@gmail.com>
2021-11-02 12:00:09 +0700

    feat: add abstract class karyawan

--- Karyawan.php
+++ Karyawan.php
@@ -1,3 +1,72 @@
-abstract class Karyawan {
+<?php
 
+class Gender {
+    const MALE = 'laki-laki';
+    const FEMALE = 'Perempuan';
+}
+class LevelKaryawan {
+    const JUNIOR = 2000000;
+    const AMATEUR = 3500000;
+    const SENIOR = 5000000;
+}
+
+abstract class Karyawan {
+    private int $gaji = 0;
+
+    public function __construct(
+        private string $no,
+        private string $nama,
+        private string $ttl,
+        private string $gender,
+        private string $level,
+        private string $status,
+    )
+    {
+        $this->setGajiKaryawanLevel($this->level);
+    }
+
+    public function setGajiKaryawanLevel(string $level)
+    {
+        switch ($level) {
+            case LevelKaryawan::AMATEUR:
+                $this->gaji = LevelKaryawan::AMATEUR;
+            case LevelKaryawan::JUNIOR:
+                $this->gaji = LevelKaryawan::JUNIOR;
+            case LevelKaryawan::SENIOR:
+                $this->gaji = LevelKaryawan::SENIOR;
+        }
+    }
+
+    public function getNoKaryawan()
+    {
+        return $this->no;
+    }
+    public function getNamaKaryawan()
+    {
+        return $this->nama;
+    }
+    public function getTTLKaryawan()
+    {
+        return $this->ttl;
+    }
+    public function getGenderKaryawan()
+    {
+        return $this->gender;
+    }
+    public function getLevelKaryawan()
+    {
+        return $this->level;
+    }
+    public function getStatusKaryawan()
+    {
+        return $this->status;
+    }
+    public function getGajiKaryawan()
+    {
+        return $this->gaji;
+    }
+    public function setGajiKaryawan(string $gaji)
+    {
+        $this->gaji = $gaji;
+    }
 }
